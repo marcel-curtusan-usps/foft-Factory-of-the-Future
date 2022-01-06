efb716a6f33e791f10c93e2e1f74641ebb912c37
Erich Osman <erich.c.osman@usps.gov>
2022-01-06 15:15:41 -0500

    Added twenty four hour clock to own JS file.

--- Factory of the Future/Scripts/Default.js
+++ Factory of the Future/Scripts/Default.js
@@ -126,33 +126,7 @@ $(() => {
     });
     map.addControl(new timedisplay());
 
-    //Add 24 Hour Clock
-    var ClockMessage = L.Control.extend({
-        options: {
-            position: 'topright'
-        },
-        onAdd: function (map) {
-            var container = L.DomUtil.create('label');
-            container.id = "twentyfourmessage";
-            container.className = "btn btn-secondary btn-sm";
-            container.style = "width: 150px";
-            return container;
-        }
-    });
-    map.addControl(new ClockMessage());
-
-    $('#tfhcContent').click(function () { $('#twentyfourmessage').popover('hide'); });
-
-    $('#twentyfourmessage').popover({
-        html: true,
-        trigger: 'click',
-        content: $('#tfhcContent'),
-    });
-
-    $('#twentyfourmessage').on('click', function () {
-        // close the sidebar
-        sidebar.close();
-    });
+    Initiate24HourClock();
    
     map.addControl(sidebar);
     sidebar.on('content', function (ev) {
@@ -1596,48 +1570,4 @@ function pad(str, max) {
     return str.length < max ? pad("0" + str, max) : str;
 };
 
-function GetTwentyFourMessage(time) {
-    var format = 'HH:mm:ss';
-    var currtime = moment(moment(time).format(format), format);
 
-    if (currtime.isBetween(moment('00:00:00', format), moment('00:30:00', format))) {
-        return '00:30 Outgoing Secondary Completed';
-    }
-    if (currtime.isBetween(moment('00:30:00', format), moment('02:30:00', format))) {
-        return '02:30 100% FedEx and Commercial Assigned';
-    }
-    if (currtime.isBetween(moment('02:30:00', format), moment('05:00:00', format))) {
-        return '05:00 100% DPS Second Pass(919) Completed';
-    }
-    if (currtime.isBetween(moment('05:00:00', format), moment('07:00:00', format))) {
-        return '00:00-07:00 100% Trips On Time';
-    }
-    if (currtime.isBetween(moment('07:00:00', format), moment('15:00:00', format))) {
-        //if (currtime.isBetween(moment('14:30:00', format), moment('15:00:00', format))) {
-        //    document.getElementById("twentyfourmessage").style.background = "#e71921";
-        //}
-        //else {
-        //    document.getElementById("twentyfourmessage").style.background = "";
-        //}
-        return '15:00 100% MMP Mail Cleared';
-    }
-    if (currtime.isBetween(moment('15:00:00', format), moment('20:00:00', format))) {
-        return '20:00 80% Mail Canceled';
-    }
-    if (currtime.isBetween(moment('20:00:00', format), moment('00:00:00', format))) {
-        return '24:00 Outgoing Primary Completed';
-    }
-}
-
-function SetClockHands(time) {
-    var hr = moment(time).hour();
-    var min = moment(time).minute();
-    var sec = moment(time).second();
-    var hr_rotation = 15 * hr + min / 4;
-    var min_rotation = 6 * min;
-    var sec_rotation = 6 * sec;
-
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-}
