6102100317373ab50cdc2a86925b4aa956a0deda
Marcel Curtusan <marcel.curtuasn@usps.gov>
2023-03-09 12:25:28 -0500

    fix the aligement for the MPE display

--- Factory of the Future/MPE/MPE.aspx
+++ Factory of the Future/MPE/MPE.aspx
@@ -5,6 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <link href="../Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/mpe.css" rel="stylesheet" />
     <title>MPE</title>
 </head>
 <body>
@@ -13,29 +14,41 @@
             <div class="container-fluid">
                 <div class="row justify-content-md-center">
                     <div class="col-5 float-right" style="padding: 0rem;">
-                        <label class="control-label" style="font-size: 4rem;" id="mpeName_text">MPE Name:</label>
-                        <label class="control-label" style="font-size: 4rem; font-weight: bolder;" id="mpeName"></label>
+                        <label class="control-label" style="font-size: 3rem;" id="mpeName_text"></label>
+                        <label class="control-label" style="font-size: 3rem; font-weight: bolder;" id="mpeName"></label>
                     </div>
-                    <div class="col-6" style="padding: 0rem;">
-                        <label class="control-label float-right" style="font-size: 4rem;" id="opn_text">Operation Number:</label>
+                    <div class="col-5" style="padding: 0rem;">
+                        <label class="control-label float-right" style="font-size: 3rem;" id="opn_text">Operation:</label>
                     </div>
                     <div class="col" style="padding: 0rem;">
-                        <label class="control-label" style="font-size: 4rem; font-weight: bolder;" id="opn"></label>
+                        <label class="control-label float-right" style="font-size: 3rem; font-weight: bolder;" id="opn"></label>
+                    </div>
+                </div>
+                 <%-- <div class="row">
+                    <div class="col-5 float-right" style="padding: 0rem;">
+                    </div>
+                    <div class="col-5" style="padding: 0rem;">
+                        <label class="control-label float-right" style="font-size: 3rem;" id="plansortprogram">Planned Sort Program:</label>
+                    </div>
+                    <div class="col" style="padding: 0rem;">
+                        <label class="control-label" style="font-size: 3rem; font-weight: bolder;" id="plansortprogram_name_text"></label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-5 float-right" style="padding: 0rem;">
                     </div>
                     <div class="col-5" style="padding: 0rem;">
-                        <label class="control-label float-right" style="font-size: 4rem;" id="sortprogram">Sortplan Name:</label>
+                        <label class="control-label float-right" style="font-size: 3rem;" id="sortprogram">Sort Program:</label>
                     </div>
                     <div class="col" style="padding: 0rem;">
-                        <label class="control-label" style="font-size: 4rem; font-weight: bolder;" id="sortplan_name_text"></label>
+                        <label class="control-label float-right" style="font-size: 3rem; font-weight: bolder;" id="sortplan_name_text"></label>
                     </div>
-                </div>
+                </div>--%>
+               
             </div>
         </div>
         <div class="card-body" style="padding: 0px;">
+
             <div class="table-responsive">
                 <table class="table table-bordered table-hover table-striped" id="mpeStatustable">
                     <tbody>
@@ -44,15 +57,25 @@
             </div>
         </div>
         <div class="card-footer">
-            <div class="text-center" style="padding: 0rem;" id="countdowndiv">
-                <label class="control-label" style="font-size: 4rem;" id="countdowntext">Planned Sort Program End Time Countdown:</label>
-                <label class="control-label" style="font-size: 4rem; font-weight: bolder;" id="countdown"></label>
-            </div>
-            <div class="text-center">
-                <label class="control-label" style="font-size: 4rem;" id="mpe_status_txt">Machine Status:</label>
-                <label class="control-label" style="font-size: 4rem; font-weight: bolder;" id="mpe_status"></label>
-            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-10" style="padding: 0rem;">
+                        <label class="control-label float-left" style="font-size: 3rem;" id="countdowntext">Planned Sort Program End Time Countdown:</label>
 
+                    </div>
+                    <div class="col" style="padding: 0rem;">
+                        <label class="control-label float-right" style="font-size: 3rem; font-weight: bolder;" id="countdown"></label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" style="padding: 0rem;">
+                        <label class="control-label float-left" style="font-size: 3rem;" id="mpe_status_txt">Machine Status:</label>
+                    </div>
+                    <div class="col" style="padding: 0rem;">
+                        <label class="control-label float-right" style="font-size: 3rem; font-weight: bolder;" id="mpe_status"></label>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <script src="../Scripts/jquery-3.6.0.js"></script>
--- Factory of the Future/MPE/Scripts/mpe.js
+++ Factory of the Future/MPE/Scripts/mpe.js
@@ -14,6 +14,11 @@ let connecttimer = 0;
 let connectattp = 0;
 let MPEName = "";
 let fotfmanager = $.connection.FOTFManager;
+let DateTimeNow = new Date();
+let CurrentTripMin = 0;
+let CountTimer = 0;
+let TimerID = -1;
+let Timerinterval = 1;
 $.extend(fotfmanager.client, {
     updateMPEStatus: (updatedata) => { Promise.all([updateMPEStatus(updatedata)]) }
 });
@@ -24,6 +29,8 @@ async function updateMPEStatus(data)
             $('label[id=mpeName]').text(data.MpeId);
             $('label[id=opn]').text(data.cur_operation_id);
             $('label[id=sortplan_name_text]').text(data.cur_sortplan);
+            $('label[id=mpe_status]').text(MPEStatus(data));
+            Promise.all([buildDataTable(data)]);
         }
     } catch (e) {
         console.log(e);
@@ -42,7 +49,8 @@ async function LoadData()
 }
 function createMPEDataTable(table) {
     let arrayColums = [{
-        "":"",
+        "order":"",
+        "Name":"",
         "Planned": "",
         "Actual": ""
     }]
@@ -54,14 +62,21 @@ function createMPEDataTable(table) {
             tempc = {
                 "title": 'Planned',
                 "mDataProp": key,
-                "class": "row-cts-count text-center"
+                "class": "col-planned text-center"
             }
         }
         else if (/Actual/i.test(key)) {
             tempc = {
                 "title": "Actual",
                 "mDataProp": key,
-                "class": "row-cts-des text-center"
+                "class": "col-actual text-center"
+            }
+        }
+        else if (/Name/i.test(key)) {
+            tempc = {
+                "title": "",
+                "mDataProp": key,
+                "class": "col-name text-right"
             }
         }
         else {
@@ -87,13 +102,31 @@ function createMPEDataTable(table) {
         bInfo: false,
         destroy: true,
         aoColumns: columns,
+        sorting: [[0, "asc"]],
         columnDefs: [{
             visible: false,
             targets: 0,
-        }]
+        }],
+        rowCallback: function (row, data, index) {
+            $(row).find('td').css('font-size', 'calc(0.1em + 2.6vw)');
+        }
     });
 }
-function loadDataTable(data, table) {
+function updateMpeDataTable(ldata, table) {
+    let load = true;
+    if ($.fn.dataTable.isDataTable("#" + table)) {
+        $('#' + table).DataTable().rows(function (idx, data, node) {
+            load = false;
+            if (data.Name === ldata.Name) {
+                $('#' + table).DataTable().row(node).data(ldata).draw().invalidate();
+            }
+        })
+        if (load) {
+            loadMpeDataTable(ldata, table);
+        }
+    }
+}
+function loadMpeDataTable(data, table) {
     if ($.fn.dataTable.isDataTable("#" + table)) {
         if (!$.isEmptyObject(data)) {
             $('#' + table).DataTable().rows.add(data).draw();
@@ -105,6 +138,79 @@ function removeLegsTripDataTable(table) {
         $('#' + table).DataTable().rows(function (idx, data, node) {
             $('#' + table).DataTable().row(node).remove().draw();
         })
+    }
+}
+async function buildDataTable(data)
+{
+    let dataArray = [];
+   
+    $.each(data, function (key, value) {
+        let tabledataObject = {};
+        if (/cur_thruput_ophr/i.test(key)) {
+            tabledataObject = {
+                "order": 3,
+                "Name": "Throughput",
+                "Planned": data.expected_throughput,
+                "Actual": data.cur_thruput_ophr,
+            }
+          dataArray.push(tabledataObject);
+        }
+        if (/tot_sortplan_vol/i.test(key)) {
+            tabledataObject = {
+                "order": 2,
+                "Name": "Volume",
+                "Planned": data.rpg_est_vol,
+                "Actual": data.tot_sortplan_vol,
+            }
+            dataArray.push(tabledataObject);
+        }
+        if (/ars_recrej3/i.test(key)) {
+            tabledataObject = {
+                "order": 4,
+                "Name": "Reject Rate",
+                "Planned": 0,
+                "Actual": data.ars_recrej3,
+            }
+            dataArray.push(tabledataObject);
+        }
+        if (/rpg_end_dtm/i.test(key)) {
+            tabledataObject = {
+                "order": 5,
+                "Name": "End Time",
+                "Planned": VaildateEstComplete(data.rpg_end_dtm),
+                "Actual": "Estimated " + VaildateEstComplete(data.rpg_est_comp_time),
+            }
+            if (CurrentTripMin === 0) {
+                CountTimer = startTimer(data.rpg_est_comp_time);
+                dataArray.push(tabledataObject);
+            }
+        }
+        if (/rpg_end_dtm/i.test(key)) {
+            tabledataObject = {
+                "order": 1,
+                "Name": "Sort Program",
+                "Planned": data.cur_sortplan,
+                "Actual": data.cur_sortplan,
+            }
+            dataArray.push(tabledataObject);
+        }
+        //if (true) {
+        //    tabledataObject = {
+        //        "Volume": '',
+        //        "Throughput": "",
+        //        "Reject Rate": "",
+        //        "End Time": "",
+        //    }
+        //}
+    });
+    updateMpeDataTable(dataArray, "mpeStatustable");
+}
+function MPEStatus(data) {
+    if (/^(0)/i.test(data.cur_sortplan)) {
+        return "Idle";
+    }
+    else {
+        return "Running";
     }
 }
 $(function () {
@@ -170,4 +276,188 @@ function capitalize_Words(str) {
     return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
+}
+function VaildateEstComplete(estComplet) {
+    try {
+        let est = moment(estComplet);
+        if (est._isValid && est.year() === moment().year()) {
+            return est.format("MM/DD/YYYY hh:mm:ss A");
+        }
+        else {
+            return "Not Available";
+        }
+    } catch (e) {
+
+    }
+}
+function startTimer(SVdtm) {
+    if (!!SVdtm) {
+        let duration = calculatescheduledDuration(SVdtm);
+        let timer = setInterval(function () {
+            if (!!duration && duration._isValid) {
+                CurrentTripMin = duration.asMinutes();
+
+                    duration = moment.duration(duration.asSeconds() - Timerinterval, 'seconds');
+                    $('label[id=countdown]').html(duration.format("d [days] hh:mm:ss ", { trunc: true }));
+                
+            }
+            else {
+                stopTimer()
+            }
+
+        }, 1000);
+
+        TimerID = timer;
+
+        return timer;
+    }
+    else {
+        stopTimer()
+    }
+};
+function stopTimer() {
+    clearInterval(CountTimer);
+}
+function calculatescheduledDuration(t) {
+    if (!!t) {
+        let timenow = moment(DateTimeNow);
+        let conditiontime = moment(t);
+        if (conditiontime._isValid && conditiontime.year() === timenow.year()) {
+            if (timenow > conditiontime) {
+                return moment.duration(timenow.diff(conditiontime));
+            }
+            else {
+                return moment.duration(conditiontime.diff(timenow));
+            }
+        }
+        else {
+            return "";
+        }
+    }
+}
+//sample data 
+let sample_data = {
+    "mpe_type": "DBCS",
+    "mpe_number": "68",
+    "bins": "0",
+    "cur_sortplan": "LV96734U",
+    "cur_thruput_ophr": "4272",
+    "tot_sortplan_vol": "338",
+    "rpg_est_vol": "0",
+    "act_vol_plan_vol_nbr": "0",
+    "current_run_start": "2023-03-03 13:48:20",
+    "current_run_end": "0",
+    "cur_operation_id": "249",
+    "bin_full_status": "0",
+    "bin_full_bins": "",
+    "throughput_status": "3",
+    "unplan_maint_sp_status": "0",
+    "op_started_late_status": "0",
+    "op_running_late_status": "0",
+    "sortplan_wrong_status": "0",
+    "unplan_maint_sp_timer": "0",
+    "op_started_late_timer": "0",
+    "op_running_late_timer": "0",
+    "sortplan_wrong_timer": "0",
+    "hourly_data": [
+        {
+            "hour": "2023-03-03 13:00",
+            "count": 4748
+        },
+        {
+            "hour": "2023-03-03 12:00",
+            "count": 8663
+        },
+        {
+            "hour": "2023-03-03 11:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 10:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 09:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 08:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 07:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 06:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 05:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-03 04:00",
+            "count": 5516
+        },
+        {
+            "hour": "2023-03-03 03:00",
+            "count": 9895
+        },
+        {
+            "hour": "2023-03-03 02:00",
+            "count": 11514
+        },
+        {
+            "hour": "2023-03-03 01:00",
+            "count": 8401
+        },
+        {
+            "hour": "2023-03-03 00:00",
+            "count": 10234
+        },
+        {
+            "hour": "2023-03-02 23:00",
+            "count": 7439
+        },
+        {
+            "hour": "2023-03-02 22:00",
+            "count": 6544
+        },
+        {
+            "hour": "2023-03-02 21:00",
+            "count": 9227
+        },
+        {
+            "hour": "2023-03-02 20:00",
+            "count": 5937
+        },
+        {
+            "hour": "2023-03-02 19:00",
+            "count": 1366
+        },
+        {
+            "hour": "2023-03-02 18:00",
+            "count": 9387
+        },
+        {
+            "hour": "2023-03-02 17:00",
+            "count": 1035
+        },
+        {
+            "hour": "2023-03-02 16:00",
+            "count": 0
+        },
+        {
+            "hour": "2023-03-02 15:00",
+            "count": 3539
+        },
+        {
+            "hour": "2023-03-02 14:00",
+            "count": 6496
+        }
+    ],
+    "rpg_expected_thruput": "25,000",
+    "ars_recrej3": "1",
+    "sweep_recrej3": "0"
 }
