a40c3f373339774662a046eafab19421b342d0d1
Q6QR00 <vicctor.m.perezmoreno@usps.gov>
2023-05-16 13:58:40 -0400

    Group Names feature for MPEs

---
+++ Factory of the Future/Models/MPEGroupsNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Factory_of_the_Future.Models
+{
+    public class MPEGroupsNames
+    {
+        public MPEGroupsNames(List<string> mpeNames, List<string> groupNames)
+        {
+            MPENames = mpeNames;
+            GroupNames = groupNames;
+        }
+        public IEnumerable<string> MPENames { get; set; }
+        public IEnumerable<string> GroupNames { get; set; }
+    }
+}
