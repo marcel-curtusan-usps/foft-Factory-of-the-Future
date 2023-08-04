25120039d04ce4864be916f7fb3e1cd242fa6226
Marcel Curtusan <marcel.curtuasn@usps.gov>
2023-08-04 11:43:40 -0400

    fix nulls in tag data

--- Factory of the Future/Models/QuuppaTag.cs
+++ Factory of the Future/Models/QuuppaTag.cs
@@ -40,7 +40,7 @@ namespace Factory_of_the_Future.Models
         [JsonProperty("deviceType")]
         public string DeviceType { get; set; } = "";
 
-        [JsonProperty("tagName")]
+        [JsonProperty("tagName", NullValueHandling = NullValueHandling.Ignore)]
         public string TagName { get; set; } = "";
 
         [JsonProperty("tagState")]
@@ -79,7 +79,7 @@ namespace Factory_of_the_Future.Models
         [JsonProperty("triggerCount")]
         public long TriggerCount { get; set; } = 0;
 
-        [JsonProperty("triggerCountTS")]
+        [JsonProperty("triggerCountTS",NullValueHandling = NullValueHandling.Ignore)]
         public long TriggerCountTS { get; set; } = 0;
 
         [JsonProperty("coordinateSystemId")]
@@ -95,3 +95,42 @@ namespace Factory_of_the_Future.Models
         public List<string> LocationZoneNames { get; set; } = new List<string>();
     }
 }
+/* data from quuppa
+ {
+	"acceleration": null,
+	"accelerationTS": 1691158328866,
+	"batteryAlarm": "ok",
+	"batteryAlarmTS": 1691158328449,
+	"batteryVoltage": 3,
+	"batteryVoltageTS": 1691158327515,
+	"button2State": "notPushed",
+	"button2StateTS": 1691158328449,
+	"button1State": "notPushed",
+	"button1StateTS": 1691158328449,
+	"color": "#FF0000",
+	"configStatus": "notStarted",
+	"configStatusTS": 1691158328866,
+	"coordinateSystemId": "7a23124a-7bf5-48e6-afd1-24acec977410",
+	"coordinateSystemName": "Floor",
+	"deviceAddress": "efba5c8e412a",
+	"deviceType": "QT1",
+	"tagGroupName": null,
+	"tagGroupId": null,
+	"locationCoordSysName": "Floor",
+	"locationTS": 1691158327761,
+	"button2LastPressTS": null,
+	"button1LastPressTS": null,
+	"lastPacketTS": 1691158328754,
+	"tagName": null,
+	"smoothedPosition": [275.09, 48.40, 1.20],
+	"smoothedPositionAccuracy": 0.32,
+	"tagState": "triggered",
+	"tagStateTS": 1691158328451,
+	"temperature": null,
+	"temperatureTS": 1691158328867,
+	"triggerCount": 26299,
+	"triggerCountTS": 1691158328130,
+	"locationZoneIds": ["69172ee1-d563-4467-bd28-d1f5739ad026", "d2808df4-ad9e-4eed-b98a-da1207d209ca", "658987b1-741e-453a-b392-1647a4312f95"],
+	"locationZoneNames": ["APPS-068", "MMWH_Packages-E", "VP_Packages"]
+}
+ */
